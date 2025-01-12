d70e778d03b575bd50d09aa1235b46a8a80fe845
Stazzy <freef@tutamail.com>
2025-01-12 08:15:20 -0500

    Add files via upload

--- Random.js
+++ Random.js
@@ -1,1 +1,8 @@
-var sites = ['https://stazzy-drawings.pages.dev/', 'https://stazzy-digital-art.pages.dev/', 'https://stazzy-music.pages.dev/', 'https://stazzy-text.pages.dev/', 'https://stazzy-videos.pages.dev/', 'https://stazzy-games.pages.dev/']; function randomSite() {var i = parseInt(Math.random() * sites.length); location.href = sites[i];}
+var sites = [
+'https://stazzy-drawings.pages.dev/', 
+'https://stazzy-digital-art.pages.dev/', 
+'https://stazzy-music.pages.dev/', 
+'https://stazzy-text.pages.dev/', 
+'https://stazzy-videos.pages.dev/', 
+'https://stazzy-games.pages.dev/'
+]; function randomSite() {var i = parseInt(Math.random() * sites.length); location.href = sites[i];}
