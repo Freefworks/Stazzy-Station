4b2159807f5f3863261dd4a43843e243f516992e
Stazzy <freef@tutamail.com>
2025-05-30 08:49:32 -0400

    Add files via upload

---
+++ Switch.js
@@ -0,0 +1,1 @@
+var x = 1; function switchImg() {if (x == 1) {document.getElementById("imgToSwitch").src = "16 Ostrinus TP.png"; x++;} else if (x == 2) {document.getElementById("imgToSwitch").src = "17 Catsworth TP.png"; x++;} else if (x == 3) {document.getElementById("imgToSwitch").src = "1 Stazzy TP.png"; x = 1;}}
